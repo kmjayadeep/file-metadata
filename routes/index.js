95b6b21472fa87751084c90a4ca97c35674ac7f8
Jayadeep KM <kmjayadeep@gmail.com>
2016-04-04 01:11:06 +0530

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+var multer = require('multer')
+var upload = multer()
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
+
+router.post('/api/upload', upload.single('file'), function(req, res) {
+    res.json({ size: req.file.size })
+})
+
+module.exports = router;
